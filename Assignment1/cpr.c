489962cefa07746a0e11460c316b622accd5f767
James Couture <jcout071@uottawa.ca>
2022-05-28 15:38:22 -0400

    Worked on ass. 1

--- Assignment1/cpr.c
+++ Assignment1/cpr.c
@@ -1,8 +1,8 @@
 /* ------------------------------------------------ ------------
 File: cpr.c
 
-Last name:
-Student number:
+Last name: Couture
+Student number: 300076065
 
 Description: This program contains the code for creation
  of a child process and attach a pipe to it.
@@ -17,6 +17,10 @@ Explanation of the zombie process
 ------------------------------------------------------------- */
 #include <stdio.h>
 #include <sys/select.h>
+#include <string.h> 
+#include <unistd.h>
+#include <stdlib.h>
+#define MSGSIZE 17
 
 /* Prototype */
 void createChildAndRead (int);
@@ -64,6 +68,89 @@ Description:
 
 void createChildAndRead(int prcNum)
 {
+	//Create buffer
+	char inbuf[MSGSIZE];
+
+	if (prcNum == 1){
+		write(1, "Process 1 begins", MSGSIZE);
+		sleep(0);
+		write(1, "Process 1 ends", MSGSIZE);
+		return;
+	}
+
+	else {
+		//Storing variable for pipe
+		int fd[2];
+
+		int ret = pipe(fd);
+		if (ret == -1) {
+			/* error */
+			perror("pipe");
+			exit(1);
+		}
+
+		int pid = fork();
+		if (pid < 0) {
+			fprintf(stderr, "Fork Failed");
+			exit(-1);
+		}
+		else if (pid == 0) {
+			//This is child, closing read, setting up standard output to pipe
+
+			//printf("%s", "SOMETHINBGGGGGGGGGGGGGGG\n");
+
+			close(fd[0]);
+			dup2(fd[1], 1);
+
+			//Get process begin message
+			char process[] = "Process ";
+			char buffer[17];
+			sprintf(buffer, "%d", prcNum);
+			strcat(process, buffer);
+			strcat(process, " begins");
+
+			//printf("%s", process);
+			write(fd[1], process, MSGSIZE);
+			//close(fd[1]);
+
+			int answer = prcNum - 1;
+
+			char minusone[17];
+			sprintf(minusone, "%d", answer);
+			//printf("%s", minusone);
+
+			char* args[] = {"./cpr", minusone, NULL };
+
+			execvp("./cpr", args);
+
+
+		}
+		else {
+			close(fd[1]);
+			dup2(fd[0], 0);
+
+			while (read(fd[0], inbuf, MSGSIZE)) {
+				strcat(inbuf, "\n");
+				write(1, inbuf, 17);
+				//printf("%s", inbuf);
+				//printf("\n");
+			}
+			
+			//Get process end message
+			char process[] = "Process ";
+			char buffer[20];
+			sprintf(buffer, "%d", prcNum);
+			strcat(process, buffer);
+			strcat(process, " ends");
+
+			//printf("%s", process);
+			printf("%s",process);
+			//write(1, process, 15);
+
+		}
+
+
+	}
 
  /* Please complete this function according to the
 Assignment instructions. */
