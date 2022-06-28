1e3dd06b6ef891ecbfec8f28706988a855c41d2e
James Couture <jcout071@uottawa.ca>
2022-06-28 17:58:04 -0400

    added library

--- Assignment3/assignment3.c
+++ Assignment3/assignment3.c
@@ -1,3 +1,22 @@
-#include <pthread.h>
 #include <stdio.h>
+#include <sys/select.h>
+#include <string.h> 
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
 
+int main (int ac, char **av)
+{
+ int processNumber; 
+
+ if (ac == 2)
+ {
+ if (sscanf (av [1], "%d", &processNumber)== 1)
+ {
+    printf("succes");
+ }
+    else fprintf(stderr, "Cannot translate argument\n");
+ }
+    else fprintf(stderr, "Invalid arguments\n");
+    return (0);
+}
