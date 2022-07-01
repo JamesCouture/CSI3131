a5effeb2835fffb685bf39fae0cd11faf2805982
James Couture <jcout071@uottawa.ca>
2022-07-01 01:09:59 -0400

    added ta logic and mutex for student

--- Assignment3/student-ta.c
+++ Assignment3/student-ta.c
@@ -1,89 +1,119 @@
 #include <stdio.h>
 #include <sys/select.h>
-#include <string.h> 
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+
 /* Prototype */
 pthread_mutex_t lock;
-void *ta(void* param); /* the thread */
-void *student(void* param); /* the thread */
+void *ta(void *param);      /* the thread */
+void *student(void *param); /* the thread */
+int counter;
 
-int main (int ac, char **av)
+int main(int ac, char **av)
 {
- int n; 
- 
- if (ac == 2)
- {
- if (sscanf (av [1], "%d", &n)== 1)
- {
-    // create the thread here... start program
-    srand(time(NULL));
-    pthread_t tidStudent[n]; /* the thread identifier */
-    pthread_t tidTA; /* the thread identifier */
-    pthread_attr_t attrTA; /* set of attributes for the thread */
-    pthread_attr_t attrStudent; /* set of attributes for the thread */
+    int n;
 
-    // if (pthread_mutex_init(&lock, NULL) != 0)
-    // {
-    //     printf("\n mutex init failed\n");
-    //     return 1;
-    // }
+    if (ac == 2)
+    {
+        if (sscanf(av[1], "%d", &n) == 1)
+        {
+            // create the thread here... start program
+            // srand(time(NULL));
+            pthread_t tidStudent[n];    /* the thread identifier */
+            pthread_t tidTA;            /* the thread identifier */
+            pthread_attr_t attrTA;      /* set of attributes for the thread */
+            pthread_attr_t attrStudent; /* set of attributes for the thread */
 
-    /* get the default attributes */
-    pthread_attr_init(&attrTA);
-    /* get the default attributes */
-    pthread_attr_init(&attrStudent);
+            // if (pthread_mutex_init(&lock, NULL) != 0)
+            // {
+            //     printf("\n mutex init failed\n");
+            //     return 1;
+            // }
 
-    /* create the thread */
-    pthread_create(&tidTA,&attrTA,ta, NULL);
-    printf("%s","ta created\n");
-    for (int i = 0; i < n; i++) {
-    pthread_create(&tidStudent[i],&attrStudent,student, NULL);  
+            /* get the default attributes */
+            pthread_attr_init(&attrTA);
+            /* get the default attributes */
+            pthread_attr_init(&attrStudent);
+
+            /* create the thread */
+            pthread_create(&tidTA, &attrTA, ta, NULL);
+            printf("%s", "ta created\n");
+            for (int i = 0; i < n; i++)
+            {
+                pthread_create(&tidStudent[i], &attrStudent, &student, &i);
+            }
+
+            /* now wait for the thread to exit */
+            pthread_join(tidTA, NULL);
+            /* now wait for the thread to exit */
+            for (int i = 0; i < n; i++)
+            {
+                pthread_join(tidStudent[i], NULL);
+            }
+        }
+        else
+            fprintf(stderr, "Cannot translate argument\n");
     }
-
-    /* now wait for the thread to exit */
-    pthread_join(tidTA,NULL);
-    /* now wait for the thread to exit */
-    for (int i = 0; i < n; i++) {
-    pthread_join(tidStudent[i],NULL);  
-    }
-    
-    
- }
-    else fprintf(stderr, "Cannot translate argument\n");
- }
-    else fprintf(stderr, "Invalid arguments\n");
+    else
+        fprintf(stderr, "Invalid arguments\n");
     return (0);
 }
 
-
-
 /**
-* The thread will begin control in this function
-*/
-void *ta(void* param) 
+ * The thread will begin control in this function
+ */
+void *ta(void *param)
 {
-    printf("%s","in ta\n");
 
-    // help student 
+    int check = 0;
 
+    while (1 == 1)
+    {
+        if (counter != check)
+        {
+            if (counter == 1)
+            {
+                printf("%s", "Ta is working with student\n");
+            }
+            else if (counter == 0)
+            {
+                printf("%s", "Ta is going to sleep\n");
+            }
 
-    
+            check = counter;
+        }
+    }
 
-pthread_exit(0);
+    pthread_exit(0);
 }
 
+void *student(void *param)
+{
 
-void *student(void* param) 
-{   
-    
-    int r = rand() % 10;
-    printf("%s","in student\n");
-    sleep(r);
-    printf("%d\n",r );
-    // call TA
+    int a = *(int *)param;
+    // Student starting to work
+    printf("%s %d %s", "Student #", a, " is working\n");
+
+    // int r = rand() % 10;
+    // printf("%s", "in student\n");
+    sleep(1);
+    // printf("%d\n", r);
+    //  call TA
+    printf("%s %d %s", "Student #", a, " is waiting for TA\n");
+
+    counter += 1;
+    pthread_mutex_lock(&lock);
+
+    printf("%s %d %s", "Student #", a, " is working with TA\n");
+
+    sleep(2);
+    printf("%s %d %s", "Student #", a, " is done working with TA\n");
+
+    counter -= 1;
+    pthread_mutex_unlock(&lock);
 
     pthread_exit(0);
 }
